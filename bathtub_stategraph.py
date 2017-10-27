1dd9713e774d6f7032c477c8bdb3a1caac5154d3
mdhk <mdhk@users.noreply.github.com>
2017-10-27 20:38:15 +0200

    fixed trace

--- bathtub_stategraph.py
+++ bathtub_stategraph.py
@@ -5,11 +5,12 @@
 import sys
 import copy
 from graphviz import Digraph
+
 import random
 random.seed(2)
 
 def get_state ():
-  '''This let the user specify the entire initial state'''
+  '''This let the user specify the entireinitial state'''
   
   inflw_mag = raw_input("Initial magnitude of the inflow: ")
   inflw_der = raw_input("Initial derivative of the inflow: ")
@@ -23,23 +24,27 @@ def get_state ():
   outflw_der = raw_input("Initial derivative of the outflow: ")
   outflw = {"mag":  outflw_mag, "der": outflw_der}
   
-  return {"inflow": inflw, "volume": vol, "outflow": outflw}
+  pres_mag = raw_input("Initial magnitude of the pressure: ")
+  pres_der = raw_input("Initial derivative of the pressure: ")
+  pres = {"mag":  pres_mag, "der": pres_der}
+  
+  height_mag = raw_input("Initial magnitude of the height: ")
+  height_der = raw_input("Initial derivative of the height: ")
+  height = {"mag":  height_mag, "der": height_der}
+  
+  return {"inflow": inflw, "volume": vol, "outflow": outflw, 'pressure': pres, 
+  'height': height}
 
 def value_cor (quant1, quant2, val, state, no_state):
   ''' Finds the state that has the value correspondance property between quant1
   and quant2, from state, that is not no_state'''
   
-  if state[quant1]['mag'] != state[quant2]['mag']:
+  if state[quant1]['mag'] != state[quant2]['mag'] and (
+    state[quant1]['mag'] == val or state[quant2]['mag'] == val):
     if no_state[quant1]['mag'] == state[quant1]['mag']:
       state[quant1]['mag'] = state[quant2]['mag']
     else:
       state[quant2]['mag'] = state[quant1]['mag']
-
-  if state[quant1]['der'] != state[quant2]['der']:
-    if no_state[quant1]['der'] == state[quant1]['der']:
-      state[quant1]['der'] = state[quant2]['der']
-    else:
-      state[quant2]['der'] = state[quant1]['der']
   
   return state
 
@@ -53,13 +58,24 @@ def stabilise_state (state, no_state):
   # Volume P+ outflow
   state['outflow']['der'] = state['volume']['der']
   
+  # Height P+ pressure
+  state['pressure']['der'] = state['height']['der']
+  
+  # Height P+ pressure
+  state['height']['der'] = state['volume']['der']
+  
+  # Pressure P+ outflow
+  state['outflow']['der'] = state['pressure']['der']
+  
   # Value correspondances between outflow and volume
-  state = value_cor('volume', 'height', '0', state, no_state)
-  state = value_cor('height', 'pressure', '0', state, no_state)
+  state = value_cor('volume', 'outflow', '0', state, no_state)
+  state = value_cor('volume', 'outflow', 'M', state, no_state)
+  
   state = value_cor('pressure', 'outflow', '0', state, no_state)
-  state = value_cor('volume', 'height', 'M', state, no_state)
-  state = value_cor('height', 'pressure', 'M', state, no_state)
   state = value_cor('pressure', 'outflow', 'M', state, no_state)
+  
+  state = value_cor('pressure', 'height', '0', state, no_state)
+  state = value_cor('pressure', 'height', 'M', state, no_state)
   
   # Inflow I+ volume and  Outflow I- vloume
   if state['inflow']['mag'] == '+': 
@@ -73,14 +89,7 @@ def stabilise_state (state, no_state):
     if  state['outflow']['mag'] == '0':
       state['volume']['der'] = '0'
     else:
-      state['volume']['der'] = '-'
-
-  state = value_cor('volume', 'height', '0', state, no_state)
-  state = value_cor('height', 'pressure', '0', state, no_state)
-  state = value_cor('pressure', 'outflow', '0', state, no_state)
-  state = value_cor('volume', 'height', 'M', state, no_state)
-  state = value_cor('height', 'pressure', 'M', state, no_state)
-  state = value_cor('pressure', 'outflow', 'M', state, no_state)
+      state['volume']['der'] = '-' 
   
   if old_state != state:
     state = stabilise_state (state, no_state)
@@ -105,69 +114,18 @@ def add_stabilised_state (l, state, quant_name, quant_val):
         is_valid = 0
       elif q_val['mag'] == 'M' and q_val['der'] == '+':
         is_valid = 0
-    if new_state['inflow'] == {'mag': '0', 'der': '0'} and new_state['volume']['der'] == '+':
-        is_valid = 0
-    if new_state['inflow']['der'] == '+' and new_state['volume']['mag'] == 'M':
-        is_valid = 0
-    if new_state['inflow'] == {'mag': '0', 'der': '0'} and new_state['volume'] == {'mag': '+', 'der': '0'} :
-        is_valid = 0
-    if new_state['inflow'] == {'mag': '+', 'der': '+'} and new_state['volume']['der'] == '-':
-        is_valid = 0
-    if new_state['inflow'] == {'mag': '+', 'der': '0'} and new_state['volume'] == {'mag': '0', 'der': '+'}:
-        is_valid = 0
-    if new_state['inflow'] == {'mag': '+', 'der': '0'} and new_state['volume'] == {'mag': '+', 'der': '-'}:
-        is_valid = 0
-    if new_state['inflow'] == {'mag': '+', 'der': '-'} and new_state['volume'] == {'mag': '0', 'der': '+'}:
-        is_valid = 0
-    if new_state['inflow'] == {'mag': '+', 'der': '0'} and new_state['volume'] == {'mag': 'M', 'der': '-'}:
-        is_valid = 0
-    if new_state['inflow'] == {'mag': '0', 'der': '0'} and new_state['volume'] == {'mag': 'M', 'der': '-'}:
-        is_valid = 0
-    if new_state['inflow'] == {'mag': '+', 'der': '-'} and new_state['volume'] == {'mag': '+', 'der': '+'}:
-        is_valid = 0
-    if new_state['inflow'] == {'mag': '0', 'der': '+'} and new_state['volume'] == {'mag': '+', 'der': '0'}:
-        is_valid = 0
+        
     if is_valid:
       l.append(new_state)
   
   return l
 
-def illegal_transition(old_state, new_state):
-  if old_state['inflow'] == {'mag': '+', 'der': '+'} and old_state['volume'] == {'mag': '+', 'der': '+'} \
-          and new_state['inflow'] == {'mag': '+', 'der': '+'} and new_state['volume'] == {'mag': '+', 'der': '0'}:
-    return True
-  if old_state['inflow'] == {'mag': '+', 'der': '0'} and old_state['volume'] == {'mag': '+', 'der': '0'} \
-          and new_state['inflow'] == {'mag': '+', 'der': '0'} and new_state['volume'] == {'mag': '+', 'der': '+'}:
-    return True
-  if old_state['inflow'] == {'mag': '+', 'der': '-'} and old_state['volume'] == {'mag': 'M', 'der': '0'} \
-          and new_state['inflow'] == {'mag': '+', 'der': '0'} and new_state['volume'] == {'mag': 'M', 'der': '0'}:
-    return True
-  if old_state['inflow'] == {'mag': '+', 'der': '+'} and old_state['volume'] == {'mag': '+', 'der': '0'} \
-          and new_state['inflow'] == {'mag': '+', 'der': '0'} and new_state['volume'] == {'mag': '+', 'der': '0'}:
-    return True
-  if old_state['inflow'] == {'mag': '+', 'der': '-'} and old_state['volume'] == {'mag': '+', 'der': '-'} \
-          and new_state['inflow'] == {'mag': '+', 'der': '-'} and new_state['volume'] == {'mag': '+', 'der': '0'}:
-    return True
-  if old_state['inflow'] == {'mag': '+', 'der': '-'} and old_state['volume'] == {'mag': '+', 'der': '0'} \
-          and new_state['inflow'] == {'mag': '+', 'der': '0'} and new_state['volume'] == {'mag': '+', 'der': '0'}:
-    return True
-  if old_state['inflow'] == {'mag': '+', 'der': '-'} and old_state['volume'] == {'mag': '+', 'der': '0'} \
-          and new_state['inflow'] == {'mag': '+', 'der': '-'} and new_state['volume'] == {'mag': '+', 'der': '+'}:
-    return True
-  if old_state['inflow'] == {'mag': '+', 'der': '-'} and old_state['volume'] == {'mag': 'M', 'der': '0'} \
-          and new_state['inflow'] == {'mag': '0', 'der': '0'} and new_state['volume'] == {'mag': 'M', 'der': '-'}:
-    return True
-  if old_state['inflow']['der'] == '+' and new_state['inflow']['mag'] == '0':
-    return True
-  if old_state['volume']['der'] == '+' and new_state['volume']['der'] == '-':
-    return True
-  return False
-
 def next_state (state):
   '''Find possible next states from the current ones'''
   new_states = []
   
-  # Add new state if derivative is positive, but value is 0
+  # Add new state if derivative is positive/negative, but value is 0/M
+  # Those are immediate moves following previous steps
   for i, quanty in state.items():
     if quanty['der'] == '+' and quanty['mag'] == '0':
       new_states = add_stabilised_state(new_states, state, i, 
@@ -176,86 +134,100 @@ def next_state (state):
       new_states = add_stabilised_state(new_states, state, i, 
         {'mag': '+', 'der':'-'})
   
+  # If there are no new states already, then
+  if len(new_states) == 0:
+    # The derivative of the volume may change from 0 because the inflow varries
+    # e.g. if inflow has (+, -) maybe it is not "so positive" any more, so the 
+    #      influence on the vloume decreases
+    if state['volume']['der'] == '0':
+      if state['inflow']['der'] == '-':
+        next_val = copy.deepcopy(state['volume'])
+        next_val['der'] = '-'
+        new_states = add_stabilised_state(new_states, state, 'volume', next_val)
+      elif state['inflow']['der'] == '+':
+        next_val = copy.deepcopy(state['volume'])
+        next_val['der'] = '+'
+        new_states = add_stabilised_state(new_states, state, 'volume', next_val)
+  
   # If no such states can be added, then do other changes
   if len(new_states) == 0:
-    # If derivative is positive, and quantity is at 0/+, then it can go to +/max
-    # Similarly derivative is -, and quantity is +/M, then it can go to 0/+
+    # Derivatives determine changes in magnitude:
+    # If derivative is positive, and quantity is at +, then it can go to max
+    # Similarly derivative is -, and quantity is +, then it can go to 0
     for quant_name, quant_val in state.items():
       if quant_name != 'inflow'and quant_val['der'] == '+' and quant_val['mag'] == '+':
         next_val = {'mag': 'M', 'der': '0'}
         new_states = add_stabilised_state(new_states, state, quant_name, next_val)
-      elif quant_val['der'] == '+' and quant_val['mag'] == '0':
-        next_val = {'mag': '+', 'der': '+'}
         new_states = add_stabilised_state(new_states, state, quant_name, next_val)
       elif quant_val['der'] == '-' and quant_val['mag'] == '+':
         next_val = {'mag': '0', 'der': '0'}
         new_states = add_stabilised_state(new_states, state, quant_name, next_val)
-      elif quant_val['der'] == '-' and quant_val['mag'] == 'M':
-        next_val = {'mag': '+', 'der': '-'}
-        new_states = add_stabilised_state(new_states, state, quant_name, next_val)
-    
+
+    # Variate the inflow
     # Can change the derivative of the inflow from +/- to 0, and from 0 to either
-    for quant_name, quant_val in state.items():
-      if quant_val['der'] == '0':
-        next_val = copy.deepcopy(quant_val)
-        next_val['der'] = '+'
-        new_states = add_stabilised_state(new_states, state, quant_name, next_val)
-      
-        next_val = copy.deepcopy(quant_val)
-        next_val['der'] = '-'
-        new_states = add_stabilised_state(new_states, state, quant_name, next_val)
-      
-      else:
-        next_val = copy.deepcopy(quant_val)
-        next_val['der'] = '0'
-        new_states = add_stabilised_state(new_states, state, quant_name, next_val)
-  
+    if quant_val['der'] == '0':
+      next_val = copy.deepcopy(state['inflow'])
+      next_val['der'] = '+'
+      new_states = add_stabilised_state(new_states, state, 'inflow', next_val)
+    
+      next_val = copy.deepcopy(state['inflow'])
+      next_val['der'] = '-'
+      new_states = add_stabilised_state(new_states, state, 'inflow', next_val)
+    
+    else:
+      next_val = copy.deepcopy(state['inflow'])
+      next_val['der'] = '0'
+      new_states = add_stabilised_state(new_states, state, 'inflow', next_val)
+    
   return new_states
 
 
 
 
 # From here on, all functions are for graph generation purposes
+keys = ['inflow', 'volume', 'outflow', 'pressure', 'height']
+
 def start_state ():
   '''This returns the start state'''
   
   inflw = {"mag": '0', "der": '0'}
   vol = {"mag": '0', "der": '0'}
-  height = {"mag": '0', "der": '0'}
-  pressure = {"mag": '0', "der": '0'}
   outflw = {"mag":  '0', "der": '0'}
+  pres = {"mag": '0', "der": '0'}
+  height = {"mag":  '0', "der": '0'}
   
-  return {"inflow": inflw, "volume": vol, "height": height, "pressure": pressure, "outflow": outflw}
+  return {"inflow": inflw, "volume": vol, "outflow": outflw, 'pressure': pres,
+  'height': height}
 
-def state_label(state):
+def state_label (state):
   '''Returns the lable for the node representing that state'''
   
   label = ''
-  for quant_name, quant_val in sorted(state.items()):
+  for quant_name in keys:
+    quant_val = state[quant_name]
     label = label + quant_val['mag'] + quant_val['der']
   
   return label
 
-def state_description (state):
+def state_description (state, c = 0):
   '''Retruns the description of a state'''
   
-  descn = ''
+  if c != 0:
+    descn = 'ID = ' + str(c) + '\\n'
+  else:
+    descn = ''
+  
   line_breake = 0
-
-  descn += 'inflow: (' + state['inflow']['mag'] + ', ' + state['inflow']['der'] + ')\n'
-  descn += 'volume: (' + state['volume']['mag'] + ', ' + state['volume']['der'] + ')\n'
-  descn += 'height: (' + state['height']['mag'] + ', ' + state['height']['der'] + ')\n'
-  descn += 'pressure: (' + state['pressure']['mag'] + ', ' + state['pressure']['der'] + ')\n'
-  descn += 'outflow: (' + state['outflow']['mag'] + ', ' + state['outflow']['der'] + ')'
-
-  # for quant_name, quant_val in state.items():
-  #   if line_breake:
-  #     descn = descn + '\\n'
-  #   descn = descn + quant_name + ': (' +  quant_val['mag'] + ', ' + quant_val['der'] + ')  '
-  #   line_breake = 1
+  for quant_name in keys:
+    if line_breake:
+      descn = descn + '\n'
+    quant_val = copy.deepcopy(state[quant_name])
+    descn = descn + quant_name + ': (' +  quant_val['mag'] + ', ' + quant_val['der'] + ')  '
+    line_breake = 1
   
   return descn
-
+  
+  
 def create_trace(dot, queue):
   traced_states = []
   while len(queue):
@@ -266,7 +238,7 @@ def create_trace(dot, queue):
       next_states = next_state(cur_state)
       new_state = random.choice(next_states)
       queue.append(new_state)
-      if cur_state not in traced_states and (traced_states == [] or not illegal_transition(traced_states[-1], new_state)):
+      if cur_state not in traced_states or (traced_states == []):
         desc = state_description(new_state)
         dot.node(state_label(new_state), desc, shape='box')
         dot.edge(state_label(cur_state), state_label(new_state), color='red')
@@ -276,55 +248,50 @@ def create_trace(dot, queue):
 
   return dot, traced_states
 
-
 def graph_gen(start=start_state(), trace=False):
   '''This creates the state graph with random variation of inflow'''
-
+  
   dot = Digraph('State Graph')
-
+  
   change = 1
   vizited = []
-  in_graph = []
+  in_graph = [start]
   queue = [start]
-  dot.node(state_label(start), state_description(start))
-
+  dot.node (state_label(start), state_description(start, 1), shape = 'box')
+  
   while len(queue):
     cur_state = queue[0]
     del queue[0]
-
+    
     if cur_state not in vizited:
       vizited.append(cur_state)
-
+      
       next_states = next_state(cur_state)
       queue = queue + next_states
-
+      
       for state in next_states:
-          if state not in in_graph:
-            dot.node(state_label(state), state_description(state), shape='box')
-            in_graph.append(state)
-
-          if not illegal_transition(cur_state, state):
-            dot.edge(state_label(cur_state), state_label(state))
-
-  dot.edge('+-0++-+-+-', '+0+++0+0+0')
-  dot.edge('+++0++++++', '+0+-+0+0+0')
-  dot.edge('++++++++++', 'M0+0M0M0M0')
-  dot.render('state_graph.gv')
+        if state not in in_graph:
+          posn = len(in_graph) + 1
+          dot.node(state_label(state), state_description(state, posn), shape = 'box')
+          in_graph.append(state)
+        
+        dot.edge(state_label(cur_state), state_label(state))
+  
+  dot.render('state_graph')
 
   if trace:
     dot.clear()
     queue = [start]
     dot, traced_states = create_trace(dot, queue)
     counter = 0
-    while (traced_states == [] or traced_states[-1]['inflow']['mag'] == '+' or {'inflow' : {'mag': '+', 'der': '+'}, 'volume': {'mag': '+', 'der': '0'}, 'height': {'mag': '+', 'der': '0'}, 'pressure': {'mag': '+', 'der': '0'}, 'outflow': {'mag': '+', 'der': '0'}} in traced_states) and counter < 100:
+    while (traced_states == [] or traced_states[-1]['inflow']['mag'] == '+') and counter < 100:
       queue = [start]
       dot.clear()
       dot, traced_states = create_trace(dot, queue)
       counter += 1
     if counter >= 100:
       exit("Unknown error while creating a trace, please try again.\n(This may happen many times, sorry)")
-
-    dot.render('trace.gv')
+    dot.render('trace')
 
     for i, traced_state in enumerate(traced_states):
       if i == 0:
